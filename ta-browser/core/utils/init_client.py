57656a7bc034692086bde54b9ae2fcd502ed4bd8
Soumyajit22theagentic <soumyajit@theagentic.ai>
2025-04-10 13:52:04 +0530

    feat: changes in README.md and init_client.py file to make it more flexible

--- ta-browser/core/utils/init_client.py
+++ ta-browser/core/utils/init_client.py
@@ -1,12 +1,15 @@
 import os
 from core.utils.anthropic_client import create_client_with_retry as create_anthropic_client, AsyncAnthropic
 from core.utils.openai_client import create_client_with_retry as create_openai_client, AsyncOpenAI
+from pydantic_ai.models.anthropic import AnthropicModel
+from pydantic_ai.models.openai import OpenAIModel
 from core.utils.logger import Logger
 import agent_factory
 
 
 logger = Logger()
 
+FORCE_DIRECT_MODEL = os.getenv("FORCE_DIRECT_MODEL", None)
 
 async def initialize_client():
     """
@@ -16,6 +19,46 @@ async def initialize_client():
         tuple: (client_instance, model_instance)
     """
     try:
+
+        if FORCE_DIRECT_MODEL:
+            logger.info(f"Direct model initialization enabled: {FORCE_DIRECT_MODEL}")
+            if FORCE_DIRECT_MODEL == "anthropic":
+                # API key is hardcoded or could be fetched from another config if needed.
+                api_key = os.getenv("ANTHROPIC_API_KEY", None)
+                model_name = "claude-3-5-sonnet-20241022"
+                # model_name = "claude-3-7-sonnet-20250219"
+                config = {
+                    "api_key": api_key,
+                    "model": model_name,
+                    "max_retries": 3,
+                    "timeout": 300.0
+                }
+                client_instance = create_anthropic_client(AsyncAnthropic, config)
+                
+                model_instance = AnthropicModel(model_name=model_name, anthropic_client=client_instance)
+                logger.info("Initialized directly with Anthropich model override.")
+                return client_instance, model_instance
+
+            elif FORCE_DIRECT_MODEL == "openai":
+                api_key = os.getenv("OPENAI_API_KEY", None)
+                model_name = "gpt-4o-mini"
+                config = {
+                    "api_key": api_key,
+                    "model": model_name,
+                    "max_retries": 3,
+                    "timeout": 300.0
+                }
+                client_instance = create_openai_client(AsyncOpenAI, config)
+                model_instance = OpenAIModel(model_name=model_name, openai_client=client_instance)
+                logger.info("Initialized directly with OpenAI model override.")
+                return client_instance, model_instance
+            else:
+                logger.info(f"Invalid model name: {FORCE_DIRECT_MODEL}")
+                raise ValueError(f"Invalid model name: {FORCE_DIRECT_MODEL}")
+            
+        '''if force-firect model is not set, get the model instance from agent_factory'''
+
+
         # Get the model instance from agent_factory
         model_instance = agent_factory.get_text_model_instance()
         
