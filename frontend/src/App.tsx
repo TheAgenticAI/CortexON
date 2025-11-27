5b9416f5503aed4b565a479724da5f7c69aaf8cf
vedant-theagentic <vedant@theagentic.ai>
2025-11-27 19:16:50 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,20 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import {Layout} from "./pages/Layout";
 import {Vault} from "./pages/Vault";
+import {ConversationProvider} from "./contexts/ConversationContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/vault" element={<Vault />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ConversationProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/vault" element={<Vault />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConversationProvider>
   );
 }
 
